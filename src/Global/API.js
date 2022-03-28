922c290abb8f67fa967234465c27e685450a3e4b
Scott Bauer <59296781+Scott-Bauer@users.noreply.github.com>
2022-03-28 09:52:35 -0400

    Update API.js

--- src/Global/API.js
+++ src/Global/API.js
@@ -4,13 +4,7 @@ import axios from 'axios';
 export const endpoint = axios.create({
         baseURL : "https://api.openbrewerydb.org"
       });
-
-      export const geocode = axios.create({
-        baseURL : "https://maps.google.com/maps/api/geocode/json?address="
-      });
-
-
-      export const API_KEY='AIzaSyCM329xqD7AB_Wx4cH45G3NYxTL9SheHRo';
+export const API_KEY='AIzaSyCM329xqD7AB_Wx4cH45G3NYxTL9SheHRo';
 
 
 
